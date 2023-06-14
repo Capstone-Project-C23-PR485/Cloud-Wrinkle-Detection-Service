b6135e4a005fa50499edb8bc8e7c19e7e2e97a08
Rchmt-cmd <rachmath.059@gmail.com>
2023-06-14 12:49:42 +0800

    analysis code update

--- Analysis.py
+++ Analysis.py
@@ -7,102 +7,78 @@ from keras.applications.mobilenet_v2 import preprocess_input
 # from keras.models import load_model # for testing purposes
 from google.cloud import storage
 
-def detect_wrinkle(data, model):
-    file_path = data['name']
-    # TODO pisahkah prefix image-uploaded nya
-    file_name = file_path.split('/')[1]
-    bucket_name = data['bucket']
+def get_bounding_box(image):
+    # Placeholder implementation
+    # Replace this with your actual bounding box calculation logic
+    height, width, _ = image.shape
+    xmin = int(width * 0.25)
+    ymin = int(height * 0.25)
+    xmax = int(width * 0.75)
+    ymax = int(height * 0.75)
+    return xmin, ymin, xmax, ymax
 
+def detect_wrinkle(data, model):
+    # get the image data
+    file_path = data["name"]
+    file_name = file_path.split("/")[1]
+    bucket_name = data["bucket"]
     image_path = f"https://storage.googleapis.com/{bucket_name}/{file_path}"
 
     try:
-        # Read the input image using OpenCV
+        # pull the image from gcs
         req = urllib.request.urlopen(image_path)
         arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-        image = cv2.imdecode(arr, -1) # 'Load it as it is'
+        image = cv2.imdecode(arr, -1)  # 'load the image'
     except Exception as e:
-        print(f"DEBUG: exception when trying to read image. Error message: {e}")
-        raise Exception("Error when trying to read image")
-
-    try:
-        # Resize the image to the required dimensions
-        if image is None:
-            return ('DEBUG! Wrong path:', image_path)
-        else:
-            image = cv2.resize(image, dsize=(224,224))
-
-        # Preprocess the image for model prediction
-        input_data = preprocess_input(np.expand_dims(image, axis=0))
-    except Exception as e:
-        print(f"DEBUG: exception when trying to preprocess image. Error message: {e}")
-        raise Exception("Error when trying to preprocess image")
-
-    # Perform the prediction using the loaded model
-    try:
-        predictions = model.predict(input_data)
-    except Exception as e:
-        print(f"DEBUG: exception when trying to predict image. Error message: {e}")
-        raise Exception("Error when trying to predict image")
-
-    # Extract the acne instances and their class labels
-    detections = []
-    acne_labels = {
-        0: 'papules',
-        1: 'nodules',
-        2: 'pustules',
-        3: 'comedones'
-    }
-
-    for i, prediction in enumerate(predictions[0]):
-        if prediction > 0:
-            # acne_class = acne_labels[i]
-            confidence = float(prediction)
-
-            detections.append({
-                'class': acne_class,
-                'confidence': confidence
-            })
+        print(f"DEBUG : Exception when when pulling the image from gcs. Error : {e}")
+        raise Exception("Error when pulling the image from gcs")
     
     try:
-        # Draw bounding boxes on the image for detected acne instances
-        if len(detections) > 0:
-            for detection in detections:
-                acne_class = detection['class']
-                confidence = detection['confidence']
-
-                # Get the predicted bounding box coordinates
-                xmin, ymin, xmax, ymax = get_bounding_box(image, threshold)
-
-                # Draw the bounding box on the image with purple-red color
-                color = (203, 0, 203)  # Purple-red color (BGR format)
-                cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
-
-            # Display the output image with bounding boxes
-            image_result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-            # print("You have acne.")
-            for detection in detections:
-                acne_class = detection['class']
-                confidence = detection['confidence']
-        else:
-            image_result = image
-            acne_class = None
-            confidence = None
+        # preprocess the image
+        image_resized = cv2.resize(image, (224, 224))
+        image_normalized = image_resized / 255.0
+        image_expanded = np.expand_dims(image_normalized, axis=0)
     except Exception as e:
-        print(f"DEBUG: exception when trying to draw bounding box. Error message: {e}")
-        raise Exception("Error when trying to draw bounding box")
+        print(f"DEBUG : Exception when preprocessing image. Error : {e}")
+        raise Exception("Error when preprocessing the image")
+
+    try:
+        # Perform wrinkle detection using the model
+        result = model.predict(image_expanded)
+        confidence = result[0][0]
+
+        if confidence > 0:
+            label = 'Wrinkled'
+            color = (0, 0, 255)  # BGR format for red color
+        else:
+            label = 'Not Wrinkled'
+            color = (0, 255, 0)  # BGR format for green color
+    except Exception as e:
+        print(f"Exception when predicting image. Error : {e}")
+        raise Exception("Error when predicting image")
+
+    predicted_image = cv2.resize(image, (270, 280))
+
+    # Define bounding box coordinates
+    xmin, ymin, xmax, ymax = get_bounding_box(predicted_image)
+
+    # Draw bounding box
+    cv2.rectangle(predicted_image, (xmin, ymin), (xmax, ymax), color, 2)
+    cv2.putText(predicted_image, label, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+
+    try:
+        post_response = post_request(file_name, confidence, label, image_path)
+    except Exception as e:
+        print(f"Exception when posting request. Error {e}")
+        raise Exception("Error when posting request.")
     
     try:
-        post_response = post_request(file_name=file_name, confidence=confidence, acne_class=acne_class, file_path=image_path)
+        upload_to_gcs_response = upload_to_gcs(bucket_name, predicted_image, file_name)
     except Exception as e:
-        print(f"DEBUG: exception when trying to post request. Error message: {e}")
-        raise Exception("Error when trying to post request")
+        print(f"Exception when posting request. Error {e}")
+        raise Exception("Error when posting request.")\
 
-    try:
-        upload_response = upload_to_gcs(bucket_name, image_result, file_name)
-    except Exception as e:
-        print(f"DEBUG: exception when trying to upload to gcs. Error message: {e}")
-
-    return upload_response, post_response
+    return post_response, upload_to_gcs_response
 
 def upload_to_gcs(bucket_name, image_result, file_name):
     # gcs_bucket_name = 'public-picture-media-bucket'
@@ -114,18 +90,26 @@ def upload_to_gcs(bucket_name, image_result, file_name):
     blob.upload_from_filename(f'static/{file_name}')
     os.remove(f'static/{file_name}')
 
-def post_request(file_name, confidence, acne_class, file_path):
+def post_request(file_name, confidence, result, file_path):
     gcs_bucket_url = f"{os.getenv('BUCKET_URL')}/images_result"
     data = {
       "id": file_path,
       "data": {
           "confidence": confidence, 
-          "detectnedAcne": acne_class
+          "result": result
           },
       "image": f"{gcs_bucket_url}/{file_name}",
-      "model": "acne"
+      "model": "wrinkle"
     }
 
-    response = requests.post('https://skincheckai-api-b6zefxgbfa-et.a.run.app/machine-learning/report-analyses', data=data)
+    response = requests.post('https://skincheckai-api-b6zefxgbfa-et.a.run.app/machine-learning/report-analyses', json=data)
 
     return response;
+
+# # for testing purposes
+# model = load_model('models/wrinkle_model_mobilenetv2_V2.h5')
+# data = {
+#     'name' : 'images_uploaded/3abb7944-9d0f-4f4f-934b-2754e471a108.jpg',
+#     'bucket' : 'public-picture-media-bucket'
+# }
+# print(detect_wrinkle(data, model))
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import base64
 from keras.models import load_model
 import os
-# from Analysis import detect_acne
+from Analysis import detect_wrinkle
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
@@ -51,7 +51,7 @@ def index(request: dict):
         try:
             model = load_model('models/model.h5')
             print(f"DEBUG: {model}")
-            # detect_acne(data, model, 0.5)
+            detect_wrinkle(data, model, 0.5)
             return ("", 204)
         except Exception as e:
             print(f"DEBUG: exception when trying to detect acne. Error message: {e}")
